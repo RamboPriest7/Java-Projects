6614f59dc9b2f2f30f32cb7147a733b9df3d8847
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2022-05-26 22:09:02 -0700

    solved the puddles

---
+++ CS 482 - Analysis of Algorithms/Project 2 - Puddles/.history/Project2_20220526214655.java
@@ -0,0 +1,66 @@
+
+//Name: Christian Jarmon
+//Project #2: Puddles
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.*;
+import java.util.*;
+
+public class Project2 {
+
+    static int puddles;
+    static int[][] map;
+    static boolean runtime_check = true;
+
+    // --------------------------------
+    public int numIslands(char[][] grid) {
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        int Islands = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 0 && visited[i][j] == false) {
+                    Islands(grid, i, j, visited);
+                    Islands++;
+                }
+            }
+        }
+        return Islands;
+    }
+
+    public void Islands(char[][] grid, int i, int j, boolean[][] visited) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0' || visited[i][j] == true) {
+            return;
+        }
+        visited[i][j] = true;
+        // Travel in all direction: north, east, south and west.
+        Islands(grid, i - 1, j, visited);// North
+        Islands(grid, i, j + 1, visited);// East
+        Islands(grid, i + 1, j, visited);// South
+        Islands(grid, i, j - 1, visited);// West
+    }
+    // --------------------------------
+
+    public static void main(String[] args) throws IOException {
+        String[] sizes = Files.readAllLines(Paths.get("input.txt")).get(0).split(" ");
+        map = new int[Integer.parseInt(sizes[0])][Integer.parseInt(sizes[1])];
+
+        String[] vals = new String[Integer.parseInt(sizes[0])];
+        for (int i = 1; i < Integer.parseInt(sizes[0]) + 1; i++) {
+            vals[i - 1] = Files.readAllLines(Paths.get("input.txt")).get(i);
+        }
+
+        for (int i = 0; i < Integer.parseInt(sizes[0]); i++) {
+            String[] temp = new String[Integer.parseInt(sizes[1])];
+            temp = vals[i].split(" ");
+            for (int j = 0; j < Integer.parseInt(sizes[1]); j++) {
+                map[i][j] = Integer.parseInt(temp[j]);
+            }
+        }
+
+        System.out.println(puddles);
+
+    }// main ends
+}// class ends
+
+// Stop Case for a Puddle:
+// If current column
---
+++ CS 482 - Analysis of Algorithms/Project 2 - Puddles/.history/Project2_20220526215712.java
@@ -0,0 +1,68 @@
+
+//Name: Christian Jarmon
+//Project #2: Puddles
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.*;
+import java.util.*;
+
+public class Project2 {
+
+    static int puddles;
+    static int[][] map;
+    static boolean runtime_check = true;
+
+    // --------------------------------
+    public static void countPuddles(int[][] map) {
+        boolean[][] visited = new boolean[map.length][map[0].length];
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                if (map[i][j] == 0 && visited[i][j] == false) {
+                    puddles(map, i, j, visited);
+                    puddles++;
+                }
+            }
+        }
+        
+    }
+
+    public static void puddles(int[][] map, int i, int j, boolean[][] visited) {
+        if (!(i < 0 || i >= map.length || j < 0 || j >= map[0].length || map[i][j] == 1 || visited[i][j])) {
+            visited[i][j] = true;
+            //up
+            puddles(map, i - 1, j, visited);
+            //right
+            puddles(map, i, j + 1, visited);
+            //down
+            puddles(map, i + 1, j, visited);
+            //left
+            puddles(map, i, j - 1, visited);
+        }
+        
+    }
+    // --------------------------------
+
+    public static void main(String[] args) throws IOException {
+        String[] sizes = Files.readAllLines(Paths.get("input.txt")).get(0).split(" ");
+        map = new int[Integer.parseInt(sizes[0])][Integer.parseInt(sizes[1])];
+
+        String[] vals = new String[Integer.parseInt(sizes[0])];
+        for (int i = 1; i < Integer.parseInt(sizes[0]) + 1; i++) {
+            vals[i - 1] = Files.readAllLines(Paths.get("input.txt")).get(i);
+        }
+
+        for (int i = 0; i < Integer.parseInt(sizes[0]); i++) {
+            String[] temp = new String[Integer.parseInt(sizes[1])];
+            temp = vals[i].split(" ");
+            for (int j = 0; j < Integer.parseInt(sizes[1]); j++) {
+                map[i][j] = Integer.parseInt(temp[j]);
+            }
+        }
+
+        System.out.println(puddles);
+
+    }// main ends
+}// class ends
+
+// Stop Case for a Puddle:
+// If current column
