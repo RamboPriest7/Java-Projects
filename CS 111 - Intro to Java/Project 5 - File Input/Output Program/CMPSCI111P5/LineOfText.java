61f4cf730f0602873c3ec06e340179d64d8d7fa6
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2022-06-27 05:00:14 -0700

    Revert "more formatting"
    This reverts commit f00d56f53069e0667eacb0c8a0b54f4dd3d8efc3.

--- CS 111 - Intro to Java/Project 5 - File Input/Output Program/CMPSCI111P5/LineOfText.java
+++ CS 111 - Intro to Java/Project 5 - File Input/Output Program/CMPSCI111P5/LineOfText.java
@@ -3,81 +3,76 @@ CMPSCI 111L - Spring 2020
 Project 5 */
 
 public class LineOfText {
-  // Object Variables
-  private String[] words;
-  private int lineNumber;
-  private String space;
-
-  // Constructors
+ //Object Variables
+     private String[] words;
+     private int lineNumber;
+     private String space;
+  
+  
+ //Constructors
   public LineOfText(String sentence, int lineNumber, String space) {
-    words = sentence.split("\\s");
-    this.lineNumber = lineNumber;
-    this.space = space;
-  }// Con 1 LOT ends
-
+ words = sentence.split("\\s");
+ this.lineNumber = lineNumber;
+ this.space = space;
+}//Con 1 LOT ends
+  
   public LineOfText(LineOfText other) {
-    words = new String[other.words.length];
-    for (int i = 0; i < words.length; i++) {
-      words[i] = other.words[i] + "";
-    }
-    lineNumber = other.lineNumber;
-    space = other.space + "";
-  }// Con 2 LOT ends
-
-  // Getter/Setter Methods
-  public String getWord(int index) {
-    if (index >= 0 && index < words.length) {
-      return words[index];
-    } else {
-      return "";
-    }
+ words = new String[other.words.length];
+ for (int i = 0; i < words.length; i++) {
+  words[i] = other.words[i] + "";
+ }
+ lineNumber = other.lineNumber;
+ space = other.space + "";
+}//Con 2 LOT ends
+  
+ //Getter/Setter Methods
+    public String getWord(int index) {
+  if (index >= 0 && index < words.length) {
+   return words[index];
+  } else {
+   return "";
   }
-
-  public void setWord(int index, String newWord) {
-    if (index >= 0 && index < words.length) {
-      words[index] = newWord;
-    }
+ }
+ public void setWord(int index, String newWord) {
+  if (index >= 0 && index < words.length) {
+   words[index] = newWord;
   }
-
-  public int getLength() {
-    return words.length;
-  }
-
-  public int getLineNumber() {
-    return lineNumber;
-  }
-
-  public void setLineNumber(int lineNumber) {
-    this.lineNumber = lineNumber;
-  }
-
-  public String getSpace() {
-    return space;
-  }
-
-  public void setSpace(String space) {
-    this.space = space;
-  }
-
-  // Object Methods
+ }
+ public int getLength() {
+  return words.length;
+ }
+ public int getLineNumber() {
+  return lineNumber;
+ }
+ public void setLineNumber(int lineNumber) {
+  this.lineNumber = lineNumber;
+ }
+ public String getSpace() {
+  return space;
+ }
+ public void setSpace(String space) {
+  this.space = space;
+ }
+  
+  
+ //Object Methods
   public String toString() {
-    String result = String.format("%02d:%s", lineNumber, space);
-    for (int i = 0; i < words.length; i++) {
-      result += words[i] + space;
-    }
-    result += "(end-of-line)";
-    return result;
-  }
-
-  // go back and try to understand "%02d:%s"~~~~~~~~~~~~!!
+ String result = String.format("%02d:%s", lineNumber, space);
+ for (int i = 0; i < words.length; i++) {
+  result += words[i] + space;
+ }
+ result += "(end-of-line)";
+ return result;
+}
+  //go back and try to understand "%02d:%s"~~~~~~~~~~~~!!
   public String toSentence() {
-    String result = "";
-    for (int i = 0; i < words.length; i++) {
-      result += words[i];
-      if (i < words.length - 1) {
-        result += " ";
-      }
-    }
-    return result;
-  }// toSentence ends
-}// class LineOftext ends
+ String result = "";
+ for (int i = 0; i < words.length; i++) {
+  result += words[i];
+  if (i < words.length - 1) {
+   result += " ";
+  }
+ }
+ return result;
+}//toSentence ends
+}//class LineOftext ends
