641429ea38f80ec266fc902c651c4c5e439cd8f8
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2022-07-17 12:28:49 -0700

    ...

---
+++ collatz/.history/collatz_20220717000010.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.lang.*;
+import java.io.File; 
+import java.io.IOException;  
+
+import java.io.FileWriter;
+
+public class collatz {
+    public static void main(String[] args) throws IOException {
+      try {
+        File myObj = new File("results.txt");
+        if (myObj.createNewFile()) {
+          //System.out.println("File created: " + myObj.getName());
+        } else {
+          //System.out.println("File already exists.");
+        }
+      } catch (IOException e) {
+        System.out.println("An error occurred.");
+        e.printStackTrace();
+      }
+
+
+      FileWriter myWriter = new FileWriter("results.txt");
+     
+      for (int i = 0; i < 10; i++)
+      {
+          long number = i;
+          myWriter.write(number + ": ");
+          S
+          while (number != 1)
+          {
+              if (number % 2 == 0)
+              {
+                  number /= 2;
+                  myWriter.write(number + " ");
+              }
+              else
+              {
+                  number = (number * 3) + 1;
+                  myWriter.write(number + " ");
+              }
+          }
+          myWriter.write("\n");
+      }
+      myWriter.close();
+
+
+    }
+  }
